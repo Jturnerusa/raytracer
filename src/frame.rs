a1c1a8c68ec7298fe8776dcbd313e1fcd436e438
John Turner <jturner.usa@gmail.com>
2024-03-22 18:08:16 -0400

    create Rgba32 trait and impl for sdl2::pixel::Color and na::Vector3

--- src/frame.rs
+++ src/frame.rs
@@ -1,5 +1,35 @@
 use std::ops::{Index, IndexMut};
 
+pub trait Rgba32 {
+    fn to_rgba32(&self) -> (u8, u8, u8, u8);
+    fn from_rgba32(rgba: (u8, u8, u8, u8)) -> Self;
+}
+
+impl Rgba32 for sdl2::pixels::Color {
+    fn to_rgba32(&self) -> (u8, u8, u8, u8) {
+        (self.r, self.g, self.b, self.a)
+    }
+
+    fn from_rgba32((r, g, b, a): (u8, u8, u8, u8)) -> Self {
+        Self::RGBA(r, g, b, a)
+    }
+}
+
+impl Rgba32 for nalgebra::Vector3<f64> {
+    fn to_rgba32(&self) -> (u8, u8, u8, u8) {
+        (
+            (self.x * 255.0) as u8,
+            (self.y * 255.0) as u8,
+            (self.z * 255.0) as u8,
+            0,
+        )
+    }
+
+    fn from_rgba32((r, g, b, _): (u8, u8, u8, u8)) -> Self {
+        Self::new(r as f64 / 255.0, g as f64 / 255.0, b as f64 / 255.0)
+    }
+}
+
 pub struct FrameBuffer {
     width: usize,
     height: usize,
@@ -27,26 +57,18 @@ impl FrameBuffer {
         &self.pixel_data
     }
 
-    pub fn set_pixel(&mut self, x: usize, y: usize, color: sdl2::pixels::Color) {
+    pub fn set_pixel<T: Rgba32>(&mut self, x: usize, y: usize, color: T) {
         let start = (x + y * self.width) * 4;
         let stop = start + 4;
-        let bytes = color
-            .to_u32(
-                &sdl2::pixels::PixelFormat::try_from(sdl2::pixels::PixelFormatEnum::RGBA32)
-                    .unwrap(),
-            )
-            .to_le_bytes();
+        let (r, g, b, a) = color.to_rgba32();
+        let bytes = [r, g, b, a];
         self.pixel_data[start..stop].copy_from_slice(bytes.as_slice());
     }
 
-    pub fn get_pixel(&self, x: usize, y: usize) -> sdl2::pixels::Color {
+    pub fn get_pixel<T: Rgba32>(&self, x: usize, y: usize) -> T {
         let start = (x + y * self.width) * 4;
         let stop = start + 4;
-        let bytes: [u8; 4] = self.pixel_data[start..stop].try_into().unwrap();
-        let i = u32::from_le_bytes(bytes);
-        sdl2::pixels::Color::from_u32(
-            &sdl2::pixels::PixelFormat::try_from(sdl2::pixels::PixelFormatEnum::RGBA32).unwrap(),
-            i,
-        )
+        let [r, g, b, a]: [u8; 4] = self.pixel_data[start..stop].try_into().unwrap();
+        T::from_rgba32((r, g, b, a))
     }
 }
